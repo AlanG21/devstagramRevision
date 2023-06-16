ed9dcfc5133e63a36c40f7986d8626a6b71ce420
AlanG21 <2030280@upv.edu.mx>
2023-06-15 20:24:53 -0600

    first commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,52 @@
 @extends('layouts.app')
 
-
 @section('titulo')
 Tu Cuenta
 @endsection
 
-
 @section('contenido')
-    <div class=" flex justify-center">
-        <div class=" w-full md:w-6/12 lg:w-4/12 md:flex">
-            <div class="md:w-7/12 lg:w-6/12 px-5" >
-                <img src="{{ asset('img/usuario.svg')}}" alt="imagen usuario"/>
+<div class="flex justify-center">
+    <div class="w-full md:w-6/12 lg:w-4/12 md:flex">
+        <div class="md:w-7/12 lg:w-6/12 px-5">
+            <img src="{{ asset('img/usuario.svg')}}" alt="imagen usuario">
+        </div>
+        <div class="md:w-8/12 lg:w-6/12 px-5 md:flex-col md:justify-center">
+            <p class="text-gray-700 text-2xl">{{$user->username}}</p>
 
+            <!-- Añadir más contenido -->
+            <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
+                0
+                <span class="font-normal">Seguidores</span>
+            </p>
+            <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
+                0
+                <span class="font-normal">Siguiendo</span>
+            </p>
+            <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
+                0
+                <span class="font-normal">Post</span>
+            </p>
 
-            </div>
-            <div class=" md:w-8/12 lg:w-6/12 px-5 md:flex-col md:justify-center">
-              
-                <p class=" text-gray-700 text-2xl"> {{$user->username}}</p>
-
-                  <!-- Añadir mas contenido-->
-                  <p class="text-gray-800 text-sm mb-3 font-bold mt-5">0
-                    <span class="font-normal">Seguidores</span>
-                </p>
-                <p class="text-gray-800 text-sm mb-3 font-bold mt-5">0
-                    <span class="font-normal">Siguiendo</span>
-                </p>
-                <p class="text-gray-800 text-sm mb-3 font-bold mt-5">0
-                    <span class="font-normal">Post</span>
-                </p>
-            </div>
+            <!-- Mostrar la tabla de imágenes -->
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Descripción</th>
+                        <!-- Agrega más columnas aquí según tus necesidades -->
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                    <tr>
+                        <td>{{$post->titulo}}</td>
+                        <td>{{$post->descripcion}}</td>
+                        <!-- Agrega más columnas aquí según tus necesidades -->
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
 @endsection
